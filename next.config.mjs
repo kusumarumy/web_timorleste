c5f3279106dc5f717e331db97a8d0a52b196d07d
kusumarumy <wahyucahyaningrumsuji@gmail.com>
2026-09-02 09:08:57 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production"
+      ? { exclude: ["error", "warn"] }
+      : false,
+  },
 };
 
 export default nextConfig;
