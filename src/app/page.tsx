fae6c7e4720f4dfe311d3550f9c395ce6cd4089e
kusumarumy <wahyucahyaningrumsuji@gmail.com>
2026-09-09 11:29:24 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,46 @@ export default function Page() {
         <ControlPanel />
         <Legend />
         <StatusBar />
-        <Loader hidden={ready} />
+        "use client";
+
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import { I18nProvider } from "@/lib/i18n";
+import ControlPanel from "@/components/ControlPanel";
+import { TopBar, Legend, StatusBar, Loader } from "@/components/Chrome";
+import { MapTour } from "@/components/MapTour";
+
+const MapCanvas = dynamic(() => import("@/components/MapCanvas"), {
+  ssr: false,
+});
+
+export default function Page() {
+  const [mapReady, setMapReady] = useState(false);
+  const [loaderFinished, setLoaderFinished] = useState(false);
+
+  return (
+    <I18nProvider>
+      <main className="relative h-screen w-screen">
+
+        <MapCanvas
+          onReady={() => setMapReady(true)}
+        />
+
+        <TopBar />
+        <ControlPanel />
+        <Legend />
+        <StatusBar />
+
+        <Loader
+          hidden={mapReady}
+          onFinished={() => setLoaderFinished(true)}
+        />
+
+        {/* GeoLandscape Mini Tour */}
+        <MapTour
+          ready={mapReady && loaderFinished}
+        />
+
       </main>
     </I18nProvider>
   );
